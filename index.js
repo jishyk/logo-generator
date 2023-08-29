8e68f471c49d4f0420fac33d4d68b953ec4d0a3d
Joshua King <joshua-coding@Joshuas-MacBook-Pro.local>
2023-08-28 20:00:53 -0700

    Added Final Packages, and finish index.js so node index.js runs

--- index.js
+++ index.js
@@ -1,4 +1,61 @@
 const inquirer = require("inquirer");
-const { writeFile } = require('fs')
+const { writeFile } = require('fs').promises;
+
 const { Triangle, Circle, Square } = require("./lib/shapes.js");
 
+async function main() {
+  const answers = await inquirer.prompt([
+    {
+      type: 'input',
+      name: 'text',
+      message: 'Enter up to three characters (For the Logo)',
+    },
+    {
+      type: 'list',
+      name: 'shapeSelection',
+      message: 'Choose shape for logo.',
+      choices: ['Circle', 'Triangle', 'Square'],
+    },
+    {
+      type: 'input',
+      name: 'color',
+      message: 'Enter background color or Hex color.', 
+    },
+    {
+      type: 'input',
+      name: 'textColor',
+      message: 'Enter text color.',
+    }
+  ]);
+
+  let svgInfo;
+
+  switch (answers.shapeSelection) {
+    case 'Circle':
+      svgInfo = new Circle();
+      break;
+    case 'Triangle':
+      svgInfo = new Triangle();
+      break;
+    case 'Square':
+      svgInfo = new Square();
+      break;
+    default:
+      console.error('Invalid shape selection');
+      return;
+  }
+
+  svgInfo.setText(answers.text);
+  svgInfo.setColor(answers.color);
+  svgInfo.setTextColor(answers.textColor);
+
+  const file = './logo/logo.svg'; // Define the file path
+  try {
+    await writeFile(file, svgInfo.render());
+    console.log('Successfully created SVG');
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+main();
